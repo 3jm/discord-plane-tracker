906481b144bf72e8b3d88dcd4697050f66bfe558
Unknown <68416923+3jm@users.noreply.github.com>
2022-08-05 19:00:22 -0400

    Add files via upload

--- installer.py
+++ installer.py
@@ -1,1 +1,54 @@
-# not done yet
+import os, sys, subprocess
+
+clear = lambda: os.system('cls')
+
+def install(package: str):
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+
+def is_installed(package: str):
+    try:
+        __import__(package)
+        return True
+
+    except ImportError:
+        return False
+
+    except Exception as e:
+        print(f'Unexpceted error: {e} while checking if {package} is installed')
+        return True
+
+def pending_packages():
+    pending = []
+    with open('requirements.txt', 'rt') as f:
+        requirements = f.read().splitlines()
+        for package in requirements:
+            if package and not is_installed(package):
+                pending.append(package)
+    return pending
+
+def setup():
+    pending = pending_packages()
+    if not pending:
+        return print("All packages are installed")
+
+    print(f'{len(pending)} missing packages, install them? (y/n)')
+    if input().lower() == 'y':
+        for package in pending:
+            print(f'Installing {package}')
+            install(package)
+        print('All packages installed')
+    else:
+        print('Aborted')
+
+def main():
+    try:
+        clear()
+        setup()
+
+    except KeyboardInterrupt:
+        print('Installation cancelled by user')
+
+if __name__ == '__main__':
+    main()
+else:
+    print("Installer is not allowed to be ran, by other programs!")
